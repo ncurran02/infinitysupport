eff001cfda3b92545904be596e090c58894ea62d
Focusvity <nathan.curran10012@gmail.com>
2025-05-05 14:22:41 +1000

    add cloudflare for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import svelte from "@astrojs/svelte";
+
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  // output: "server",
+
+  output: "server",
   // site: "https://ncurran.github.io",
   base: "infinitysupport",
+
+  integrations: [svelte()],
+  adapter: cloudflare(),
 });
